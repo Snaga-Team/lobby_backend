997f455ab293316541883c2312e6ef3e0f82ede1
Ruslan <sichkaryk39@gmail.com>
2025-03-23 22:08:45 +0200

    Update ALLOWED_HOSTS setting to allow all hosts and modify requirements file

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,8 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
