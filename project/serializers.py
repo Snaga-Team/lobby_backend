701f8c9f0b28eb9df3e6fc1667f20afef4ef350b
Ruslan <sichkaryk39@gmail.com>
2025-08-08 18:17:51 +0300

    SN-36 feat: Add MemberSerializer for ProjectMember model with user info and date joined

--- project/serializers.py
+++ project/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from project.models import Project
+from project.models import Project, ProjectMember
+from accounts.serializers import ProfileSerializer
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -33,3 +34,12 @@ class ProjectSerializer(serializers.ModelSerializer):
 
         return Project.objects.create(owner=request.user, **validated_data)
 
+
+class MemberSerializer(serializers.ModelSerializer):
+    user_info = ProfileSerializer(source="user")
+    date_joined_to_workspace = serializers.DateTimeField(source="joined_at")
+
+    class Meta:
+        model = ProjectMember
+        fields = ['id', "user_info", 'project', 'is_active', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at', "user_info"]
